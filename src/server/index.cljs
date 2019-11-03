616fa093849d50eaa6703089610b7e92b183e1ae
Theophile Villard <theophile.villard@gmail.com>
2019-11-03 19:54:42 +0100

    return json array for last endpoint

--- src/server/index.cljs
+++ src/server/index.cljs
@@ -1,8 +1,17 @@
 (ns server.index
-  (:require ["firebase-functions" :as fire-fn]))
+  (:require ["firebase-functions" :as fire-fn]
+            ["firebase-admin" :as fire-admin]))
+
+(fire-admin/initializeApp)
 
 (def last-parens
   (.. fire-fn -https
-      (onRequest (fn [_ res] (.send res "heyheyheyA")))))
+      (onRequest
+       (fn [_ res]
+         (.. (fire-admin/database) (ref "parens") (orderByKey) (limitToLast 105)
+             (on "value"
+                 (fn [^js snap]
+                   (let [data (-> snap (.val) (js->clj :keywordize-keys true))]
+                     (.json res (clj->js (vals data)))))))))))
 
 (def exports #js {:lastParens last-parens})
