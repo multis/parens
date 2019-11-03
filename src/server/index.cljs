9019d3f55a9cfd8133aa5252c7f916bc1f2e6564
Theophile Villard <theophile.villard@gmail.com>
2019-11-03 13:56:59 +0100

    just triggerin'

--- src/server/index.cljs
+++ src/server/index.cljs
@@ -3,6 +3,6 @@
 
 (def last-parens
   (.. fire-fn -https
-      (onRequest (fn [_ res] (.send res "heyheyheyo")))))
+      (onRequest (fn [_ res] (.send res "heyheyheyi")))))
 
 (def exports #js {:lastParens last-parens})
